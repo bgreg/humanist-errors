fb9b36afe2fe5e5ae257c649a7f48d33aa311686
Chester Law <chesterlaw@me.com>
2014-12-14 16:13:29 -0800

    Add RSpec tests

--- lib/humanist_errors/humanist_errors.rb
+++ lib/humanist_errors/humanist_errors.rb
@@ -1,5 +1,13 @@
 # require "humanist_errors/errors/version"
+
 # module HumanistErrors
   # module Errors
+    class SyntaxError
+      def message
+        # if super =~ /undefined/ 
+          "Hi! You may have missed an argument and ended this line early with a semicolon. Here's the error from Ruby: " + super
+        # end
+      end
+    end
   # end
 # end
