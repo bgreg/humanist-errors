99e16c910c3f3c5a4dd350598f7e1fef147cabcb
bgreg <bgregmc@icloud.com>
2015-02-04 21:01:03 -0800

    Improved monkey patching of exception

--- lib/humanist_errors/exception_extensions.rb
+++ lib/humanist_errors/exception_extensions.rb
@@ -1,5 +1,5 @@
 module HumanistErrors
-  module Human
+  module ExceptionExtensions
     def to_s
       searcher = HumanistErrors::Search.new(self.class, super)
       if searcher.found_error == :no_result
--- lib/humanist_errors/monkey.rb
+++ lib/humanist_errors/monkey.rb
@@ -1,11 +1,1 @@
-class SyntaxError
-  include HumanistErrors::Human
-end
-
-class NoMethodError
-  include HumanistErrors::Human
-end
-
-class NameError
-  include HumanistErrors::Human
-end
+::Exception.prepend(HumanistErrors::ExceptionExtensions) 
