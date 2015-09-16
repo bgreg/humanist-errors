0271ca17de9e27648a1cb8879464e8a1ac46291b
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-15 21:14:34 -0700

    Cleanup

--- test/features/zero_division_error_test.rb
+++ test/features/zero_division_error_test.rb
@@ -2,12 +2,4 @@ require 'test_helper'
 
 class ZeroDivisionErrorTest < Minitest::Test
   include HumanistErrorsSupport
-
-  def test_message_for_divide_by_zero
-    # error = assert_raises(ZeroDivisionError) { eval('12/0') }
-
-    # # run thing in a new thread, capture the stack trace
-    # expected_message = HumanistErrors::MESSAGE_DICTIONARY[:zero_division_error][:divide_by_zero]
-    # assert_message(expected_message, error)
-  end
 end
