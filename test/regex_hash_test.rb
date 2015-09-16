7c32ed5c3d2d55a75c53a8a69b38648f36080c78
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-15 21:15:45 -0700

    Style fix

--- test/regex_hash_test.rb
+++ test/regex_hash_test.rb
@@ -10,6 +10,6 @@ class RegexHashTest < Minitest::Test
   def test_a_failure_when_key_is_not_found
     magic_string_hash = HumanistErrors::RegexHash[/findme/ => "hurray!"]
     error = assert_raises(NoMethodError) { magic_string_hash['1'] }
-    assert error.message.include? "Could not find Ruby message in dictionary: 1"
+    assert(error.message.include?("Could not find Ruby message in dictionary: 1"))
   end
 end
