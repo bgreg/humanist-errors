c22334f30f7461385ccbc6f99d8b56a7d679a8db
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-15 21:06:06 -0700

    Added method to run tests in a fork and capture the exception message

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,40 @@
 
 require 'humanist_errors'
 require 'humanist_errors/search'
+require 'humanist_errors/error_mapper'
 require 'minitest/focus'
 require 'minitest/emoji'
 require 'minitest/autorun'
 
 module HumanistErrorsSupport
+
   def assert_result(message)
     assert(message != :no_result, "no result was found")
   end
+
+  def assert_error_map(ruby_error, exception_symbol, error_symbol)
+    result = HumanistErrors::ERROR_MAPPER[exception_symbol][ruby_error]
+    assert_result(result)
+    assert_match(result, HumanistErrors::MESSAGE_DICTIONARY[exception_symbol][error_symbol])
+  end
+
+  def human_fork(exception)
+    reader, writer = IO.pipe
+
+    fork do
+      reader.close
+      require 'humanist_errors/monkey'
+      begin
+        yield
+      rescue exception => e
+        writer.puts e.message
+      end
+    end
+
+    writer.close
+    reader.gets(nil)
+  ensure 
+    reader.close
+  end
 end
 
-
-# integration tests should run bad ruby in a seperate thread and watch it crash
