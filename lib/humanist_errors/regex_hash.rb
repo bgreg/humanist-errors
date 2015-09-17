fcf25d59bfcc074244e9ba287c5becba8371c946
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-16 19:24:29 -0700

    here, have some code

--- lib/humanist_errors/regex_hash.rb
+++ lib/humanist_errors/regex_hash.rb
@@ -1,8 +1,9 @@
 module HumanistErrors
   class RegexHash < Hash
     def [](value)
-      result = self.select{|k| k =~ value}
+      result = self.select { |k| k =~ value }
       if result.empty?
+        #TODO: change this to NotImplementedError
         raise NoMethodError, "Could not find Ruby message in dictionary: #{value}"
       else
         result.shift[1]
