d009aeba2c49be50ebe369fe674b5113492bd11a
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-19 17:05:23 -0700

    User better error if nothing is found in the regex hash

--- test/features/syntax_error_test.rb
+++ test/features/syntax_error_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 require "stringio"
 
 class SyntaxErrorTest < Minitest::Test
-  include HumanistErrorsSupport
-
   def test_message_for_unexpected_semicolon
     error_message = human_fork(SyntaxError) { eval('1+;') }
     expected_message = HumanistErrors::MESSAGE_DICTIONARY[:syntax_error][:unexpected_semi_colon]
