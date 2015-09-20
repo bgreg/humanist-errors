2ac2ad2de8537918d763f860e35a6e860a4b3f71
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-19 19:48:05 -0700

    fix typo

--- lib/humanist_errors/colors.rb
+++ lib/humanist_errors/colors.rb
@@ -1,35 +1,35 @@
 module HumanistErrors
   class Color
 
-    def black(sting)
+    def black(string)
       "\033[30m#{string}\033[0m"
     end
 
-    def red(sting)
+    def red(string)
       "\033[31m#{string}\033[0m"
     end
 
-    def green(sting)
+    def green(string)
       "\033[32m#{string}\033[0m"
     end
 
-    def brown(sting)
+    def brown(string)
       "\033[33m#{string}\033[0m"
     end
 
-    def blue(sting)
+    def blue(string)
       "\033[34m#{string}\033[0m"
     end
 
-    def magenta(sting)
+    def magenta(string)
       "\033[35m#{string}\033[0m"
     end
 
-    def cyan(sting)
+    def cyan(string)
       "\033[36m#{string}\033[0m"
     end
 
-    def gray(sting)
+    def gray(string)
       "\033[37m#{string}\033[0m"
     end
 
