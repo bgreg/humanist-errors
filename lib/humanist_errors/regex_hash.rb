1a21eb66820482ba3c5034095b4231e9e092d094
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-19 17:03:00 -0700

    Use better error here

--- lib/humanist_errors/regex_hash.rb
+++ lib/humanist_errors/regex_hash.rb
@@ -3,8 +3,7 @@ module HumanistErrors
     def [](value)
       result = self.select { |k| k =~ value }
       if result.empty?
-        #TODO: change this to NotImplementedError
-        raise NoMethodError, "Could not find Ruby message in dictionary: #{value}"
+        raise NotImplementedError, "Could not find Ruby message in dictionary: #{value}"
       else
         result.shift[1]
       end
