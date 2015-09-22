574417709b82e0af7f45152108c9e082233d9eba
Greg McGuirk <bert.mcguirk@gmail.com>
2015-09-22 08:20:22 -0700

    Formatting change

--- lib/humanist_errors/exception_extensions.rb
+++ lib/humanist_errors/exception_extensions.rb
@@ -2,14 +2,13 @@ module HumanistErrors
   module ExceptionExtensions
     def to_s
       searcher = Search.run(self.class, super)
-      if searcher.found_error == :no_result
+      if :no_result == searcher.found_error
         super
       else
         color = Color.new
-        "\n#{STARTING_TOKEN}" +
-        "\t#{color.cyan(searcher.found_error)}\n" +
-        "#{ENDING_TOKEN}" +
-        super
+        "\n#{STARTING_TOKEN}" \
+        "\t#{color.cyan(searcher.found_error)}\n" \
+        "#{ENDING_TOKEN}" + super
       end
     end
   end
